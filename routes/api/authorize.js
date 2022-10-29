ac8c0465ec0678115899e027ae94cd3378571d0a
Ara Z Baghdassarian <azbaghda@gmail.com>
2022-10-29 10:48:07 -0700

    update mfa route

--- routes/api/authorize.js
+++ routes/api/authorize.js
@@ -24,7 +24,7 @@ route.post('/mfa-attempt', cors(true), async (req, res) => {
     let mfa_code = req.body.mfa_code;
     let authenticated = await authenticateToken(token, mfa_code);
     if (authenticated) return res.status(200).send(true).end();
-    return res.status(404).send(false).end();
+    return res.status(401).send(false).end();
 });
 
 route.post('/new-mongo-chart-token', cors(true), async (req, res) => {
